1e96df2204536dd6337825aa583faaa65a737f4e
Soedar <soedar.sg@gmail.com>
2013-04-17 01:06:29 -0700

    Added coins in the GameViewController

--- Stacker/GameViewController.m
+++ Stacker/GameViewController.m
@@ -11,20 +11,27 @@
 
 @interface GameViewController () <StackerViewDelegate>
 @property (nonatomic, strong) StackerView *stackerView;
+@property (nonatomic, weak) IBOutlet UILabel *coinsLabel;
+
+@property (nonatomic) int coins;
 
 @end
 
 @implementation GameViewController
 
+- (id) initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+    }
+    return self;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    
-    /*
-    self.row = [[StackerRow alloc] initWithCount:1];
-    [self.view addSubview:self.row];
-     */
+    self.coins = 3;
     
     [self initStackerView];
 }
@@ -35,27 +42,42 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)stopRow:(id)sender
-{
-    [self.stackerView moveToNextRow];
-}
-- (IBAction)reset:(id)sender
-{
-    [self.stackerView removeFromSuperview];
-    [self initStackerView];
-}
 
 - (void) initStackerView
 {
     NSArray *levelInfo = [NSArray arrayWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"StackerLevel" ofType:@"plist"]];
     
     self.stackerView = [[StackerView alloc] initWithLevel:levelInfo];
+    
+    CGRect frame = self.stackerView.frame;
+    frame.origin.y = 50;
+    self.stackerView.frame = frame;
+    
+    self.stackerView.center = CGPointMake(self.view.center.x, self.stackerView.center.y);
     self.stackerView.delegate = self;
     [self.view addSubview:self.stackerView];
     
     [self.stackerView start];
 }
 
+- (void) setCoins:(int)coins
+{
+    _coins = coins;
+    self.coinsLabel.text = [NSString stringWithFormat:@"x %i", coins];
+}
+
+#pragma mark - Button action
+
+- (IBAction)stopRow:(id)sender
+{
+    [self.stackerView moveToNextRow];
+}
+
+- (IBAction)reset:(id)sender
+{
+    [self.stackerView removeFromSuperview];
+    [self initStackerView];
+}
 
 #pragma mark - Delegate method
 
