3ec2e31449d3bf28a89683a0fd21335408be4892
Soedar <soedar.sg@gmail.com>
2013-05-26 22:02:26 -0700

    Updated the interface for the deal controller

--- Stacker/PBDealDetailsViewController.m
+++ Stacker/PBDealDetailsViewController.m
@@ -8,9 +8,21 @@
 
 #import "PBDealDetailsViewController.h"
 
+#define CONTENT_PADDING     20;
+
 @interface PBDealDetailsViewController ()
 
-@property (nonatomic, weak) IBOutlet UIImageView *imageView;
+@property (nonatomic, strong) UIView *contentView;
+@property (nonatomic, weak) IBOutlet UIScrollView *scrollView;
+@property (nonatomic, weak) IBOutlet UIView *descriptionView;
+@property (nonatomic, weak) IBOutlet UILabel *descriptionLabel;
+
+@property (nonatomic, weak) IBOutlet UIView *purchaseInfoView;
+@property (nonatomic, weak) IBOutlet UILabel *purchaseInfoLabel;
+
+@property (nonatomic, weak) IBOutlet UIView *purchaseButtonView;
+@property (nonatomic, weak) IBOutlet UIButton *purchaseButton;
+
 
 @end
 
@@ -22,6 +34,7 @@
     if (self) {
         _deal = deal;
         self.title = deal.name;
+        self.contentView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 0)];
     }
     
     return self;
@@ -32,13 +45,67 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
-    self.imageView.image = self.deal.image;
+    [self appendViewToContentView:[self viewForPurchaseButton]];
+    [self appendViewToContentView:[self viewForImage]];
+    [self appendViewToContentView:[self viewForPurchaseInfo]];
+    [self appendViewToContentView:[self viewForDescription]];
+    
+    [self.scrollView addSubview:self.contentView];
+    self.scrollView.contentSize = self.contentView.frame.size;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) appendViewToContentView:(UIView*)view
+{
+    CGRect viewFrame = view.frame;
+    CGRect contentFrame = self.contentView.frame;
+    
+    viewFrame.origin.y = contentFrame.size.height + CONTENT_PADDING;
+    view.frame = viewFrame;
+    
+    contentFrame.size.height += view.frame.size.height + CONTENT_PADDING;
+    self.contentView.frame = contentFrame;
+    
+    [self.contentView addSubview:view];
+}
+
+#pragma mark - Sections of view
+
+- (UIView *) viewForImage
+{
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:self.deal.image];
+    imageView.center = CGPointMake(160, imageView.center.x);
+    
+    imageView.backgroundColor = [UIColor redColor];
+    return imageView;
+}
+
+- (UIView *) viewForPurchaseButton
+{
+    NSString *buttonTitle = [NSString stringWithFormat:@"$%i", self.deal.cost];
+    [self.purchaseButton setTitle:buttonTitle forState:UIControlStateNormal];
+    
+    return self.purchaseButtonView;
+}
+
+- (UIView*) viewForPurchaseInfo
+{
+    self.purchaseInfoLabel.text = [NSString stringWithFormat:@"Purchase this for $%i and get %i x ", self.deal.cost, self.deal.life];
+    
+    return self.purchaseInfoView;
+}
+
+- (UIView*) viewForDescription
+{
+    self.descriptionLabel.text = self.deal.dealDescription;
+    [self.descriptionLabel sizeToFit];
+    
+    return self.descriptionView;
 }
 
 @end
